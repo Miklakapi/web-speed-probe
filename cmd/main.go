bab85d364b822a9c485c62eee284fe55852a6e14
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-06-07 11:29:11 +0200

    download endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 )
 
 const PORT = ":8080"
+const DEFAULT_DOWNLOAD_SIZE = 50_000_000
+
+type zeroReader struct{}
+
+func (zeroReader) Read(buffer []byte) (int, error) {
+	for i := range buffer {
+		buffer[i] = 0
+	}
+
+	return len(buffer), nil
+}
 
 func main() {
 	appCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
@@ -23,6 +36,35 @@ func main() {
 	mux.Handle("/", fileServer)
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNoContent)
+	})
+	mux.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		size := int64(DEFAULT_DOWNLOAD_SIZE)
+		sizeParam := r.URL.Query().Get("size")
+
+		if sizeParam != "" {
+			parsedSize, err := strconv.ParseInt(sizeParam, 10, 64)
+			if err != nil {
+				http.Error(w, "Invalid size parameter", http.StatusBadRequest)
+				return
+			}
+
+			if parsedSize <= 0 {
+				http.Error(w, "Size must be greater than zero", http.StatusBadRequest)
+				return
+			}
+
+			size = parsedSize
+		}
+
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Header().Set("Content-Length", strconv.FormatInt(size, 10))
+		w.Header().Set("Cache-Control", "no-store")
+		w.WriteHeader(http.StatusOK)
+
+		_, err := io.CopyN(w, zeroReader{}, size)
+		if err != nil {
+			log.Println(err)
+		}
 	})
 
 	srv := &http.Server{
